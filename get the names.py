e64a732d7fb0e45b3bee125e87f53a6e84c322e0
Flying Bear <Galka1999@gmail.com>
2017-08-27 16:59:21 +0300

    audio files

--- get the names.py
+++ get the names.py
@@ -9,16 +9,17 @@ def create_filenames_list(directory):
     filenames_list = []
     for root, dirs, files in os.walk(directory):
         for f in files:
-            filenames_list.append(f)
+            filenames_list.append(os.path.join(directory,f))
     return filenames_list
 
 
 def write_out_filenames_list (filenames_list, foldername):
     with open('{} filenames list.js'.format(foldername), 'w', encoding='utf-8') as n:
         text = 'var fileSrcArray = ['
-        for f in filenames_list[:-2]:
+        filenames_list = sorted(filenames_list, key=lambda x: int(x.split('\\')[-1].split('_')[0]))
+        for f in filenames_list[:-1]:
             text += '"' + f + '", '
-        text += '"' + filenames_list[-1] + '", '
+        text += '"' + filenames_list[-1] + '"]'
         n.write(text)
 
 
